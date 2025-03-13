dc75103ab32481a04abd546b6fb2dee10ca44f6f
NNine <ninelnw55tv@gmail.com>
2025-03-13 19:13:32 +0700

    Web API: add tokenController and login route

--- ToDo/DTOs/Login.cs
+++ ToDo/DTOs/Login.cs
@@ -5,7 +5,7 @@ namespace ToDo.DTOs
 {
     public class Login
     {
-        public string Id { get; set; }
+        public string NationalId { get; set; }
         public string Password { get; set; }
     }
 }
