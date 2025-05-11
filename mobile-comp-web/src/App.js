1a2885a4a0ff55cb3cbfbe152c12a0c3cbe4bde4
Camppe <camp555555@gmail.com>
2025-05-11 22:18:33 +0700

    web app

--- mobile-comp-web/src/App.js
+++ mobile-comp-web/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Main from "./pages/Main";
 import Credit from "./pages/Credit";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/main" element={<Main />} />
       <Route path="/credit" element={<Credit />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
